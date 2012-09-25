bdb12470df48f645544702b9198f0b169be2fffb
Anastasia Charushina <stasia-a@tut.by>
2012-09-25 13:25:37 +0300

    Rails initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicAlbums::Application.config.secret_token = '34217faece4ca136765530918275e67d82f503ad3dd113b8429f39088dad3438cf1b0461e076cf82ce27bea5ac7f12cc1404154fa70c55dda31b2d34a807976b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MusicAlbums::Application.config.session_store :cookie_store, key: '_music_albums_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MusicAlbums::Application.config.session_store :active_record_store
