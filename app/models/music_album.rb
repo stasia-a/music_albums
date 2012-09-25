0c674117baebea2d51c235830803e840b7958589
Anastasia Charushina <stasia-a@tut.by>
2012-09-25 13:47:11 +0300

    Validation added

--- app/models/music_album.rb
+++ app/models/music_album.rb
@@ -1,3 +1,6 @@
 class MusicAlbum < ActiveRecord::Base
   attr_accessible :name, :year
+
+  validates :duration, numericality: { only_integer: true, greater_then: 0}, length: {is: 4}, allow_blank: true
+  validates :name, presence: true
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,6 @@
 class Track < ActiveRecord::Base
   attr_accessible :duration, :name
+
+  validates :name, presence: true
+  validates :duration, numericality: { only_integer: true, greater_then: 0}, allow_blank: true
 end
